53d042ffdce2aa23f9ef0638251d6cc136c1defb
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-09 17:36:56 +0200

    feat: add tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 //mod arithmetic;
 mod boole;
+mod set;
 //mod gray_code;
 
 //use arithmetic::adder::adder;
 //use arithmetic::multiplier::multiplier;
 //use boole::boolean_evaluation::eval_formula;
 use boole::truth_table::print_truth_table;
+use set::powerset::powerset;
 //use gray_code::gray_code;
 
 fn main() {
