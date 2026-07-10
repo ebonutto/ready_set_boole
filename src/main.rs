51c5768f99e8c1013eed8e23a6796a614bb631d6
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-10 17:26:19 +0200

    feat: add tests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 //mod arithmetic;
-mod boole;
-mod set;
+//mod boole;
+//mod set;
+mod space;
 //mod gray_code;
 
 //use arithmetic::adder::adder;
 //use arithmetic::multiplier::multiplier;
 //use boole::boolean_evaluation::eval_formula;
-use boole::truth_table::print_truth_table;
-use set::powerset::powerset;
+//use boole::truth_table::print_truth_table;
+//use set::powerset::powerset;
+use space::curve::map;
 //use gray_code::gray_code;
 
 fn main() {
-    println!("Hello, world!");
-    print_truth_table("AB&C|");
+    println!("{}", map(65535, 65535));
 }
