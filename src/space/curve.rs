9c74026eb45127e2111d869db4036adcd1c582ec
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-10 16:42:20 +0200

    feat: add curve

--- src/space/curve.rs
+++ src/space/curve.rs
@@ -1,4 +1,10 @@
 pub fn map(x: u16, y: u16) -> f64 {
-    let mut result: u32;
-    
+    let mut z: u32 = 0;
+
+    for i in 0..16 {
+        z |= (((x >> i) & 1) as u32) << (2 * i);
+        z |= (((y >> i) & 1) as u32) << (2 * i + 1);
+    }
+
+    z as f64 / u32::MAX as f64
 }
