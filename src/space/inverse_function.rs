3cb8d561060073e5012c63c90abdc8e76aeb376f
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-10 17:16:24 +0200

    feat: add inverse_function

--- src/space/inverse_function.rs
+++ src/space/inverse_function.rs
@@ -1,3 +1,18 @@
 pub fn reverse_map(n: f64) -> (u16, u16) {
-    
+    assert!(
+        (0.0..=1.0).contains(&n),
+        "reverse_map: map must be between 0 and 1 ({})",
+        n
+    );
+    let z = (n * u32::MAX as f64) as u32;
+
+    let mut x: u16 = 0;
+    let mut y: u16 = 0;
+
+    for i in 0..16 {
+        y |= (((z >> (2 * i + 1)) & 1) as u16) << i;
+        x |= (((z >> (2 * i)) & 1) as u16) << i;
+    }
+
+    (x, y)
 }
