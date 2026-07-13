fd093ba8fb9432c90b05ff302a41ea46145224ba
Elio Bonutto <elio@mbp-de-elio.home>
2026-07-14 00:26:42 +0200

    fix: code working

--- src/boole/negation_normal_form.rs
+++ src/boole/negation_normal_form.rs
@@ -1,7 +1,7 @@
-use super::boole::{Formula, parse, to_nnf, to_rpn};
+use super::boole::{parse, to_nnf, to_rpn};
 
 pub fn negation_normal_form(formula: &str) -> String {
     let ast = parse(formula);
     let nnf = to_nnf(ast);
-    to_rpn(&nnf_ast)
+    to_rpn(&nnf)
 }
