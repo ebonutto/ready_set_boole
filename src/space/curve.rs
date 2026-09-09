a757efcecae7b8ab3f7f17b9eafb4724d7cf9456
Elio Bonutto <elio@MacBook-Pro-de-Elio.local>
2026-09-09 16:52:08 +0200

    feat: add tests

--- src/space/curve.rs
+++ src/space/curve.rs
@@ -6,7 +6,6 @@ pub fn map(x: u16, y: u16) -> f64 {
         z |= (((x >> i) & 1) as u32) << (2 * i);
         z |= (((y >> i) & 1) as u32) << (2 * i + 1);
     }
-
     z as f64 / u32::MAX as f64
 }
 
@@ -15,26 +14,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_extremes() {
+    fn extremes() {
         assert_eq!(map(0, 0), 0.0);
         assert_eq!(map(u16::MAX, u16::MAX), 1.0);
     }
 
     #[test]
-    fn test_single_bits() {
+    fn bit_interleaving() {
         assert_eq!(map(1, 0), 1.0 / u32::MAX as f64);
         assert_eq!(map(0, 1), 2.0 / u32::MAX as f64);
         assert_eq!(map(1, 1), 3.0 / u32::MAX as f64);
-    }
-
-    #[test]
-    fn test_first_shift() {
         assert_eq!(map(2, 0), 4.0 / u32::MAX as f64);
         assert_eq!(map(0, 2), 8.0 / u32::MAX as f64);
     }
 
     #[test]
-    fn test_in_range() {
+    fn in_range() {
         let n = map(12345, 54321);
         assert!(n >= 0.0);
         assert!(n <= 1.0);
--- src/space/inverse_function.rs
+++ src/space/inverse_function.rs
@@ -15,7 +15,6 @@ pub fn reverse_map(n: f64) -> (u16, u16) {
         y |= (((z >> (2 * i + 1)) & 1) as u16) << i;
         x |= (((z >> (2 * i)) & 1) as u16) << i;
     }
-
     (x, y)
 }
 
@@ -24,21 +23,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_extremes() {
+    fn extremes() {
         assert_eq!(reverse_map(0.0), (0, 0));
         assert_eq!(reverse_map(1.0), (u16::MAX, u16::MAX));
     }
 
     #[test]
-    fn test_single_bits() {
+    fn bit_interleaving() {
         assert_eq!(reverse_map(1.0 / u32::MAX as f64), (1, 0));
         assert_eq!(reverse_map(2.0 / u32::MAX as f64), (0, 1));
         assert_eq!(reverse_map(3.0 / u32::MAX as f64), (1, 1));
+        assert_eq!(reverse_map(4.0 / u32::MAX as f64), (2, 0));
+        assert_eq!(reverse_map(8.0 / u32::MAX as f64), (0, 2));
     }
 
     #[test]
-    fn test_first_shift() {
-        assert_eq!(reverse_map(4.0 / u32::MAX as f64), (2, 0));
-        assert_eq!(reverse_map(8.0 / u32::MAX as f64), (0, 2));
+    #[should_panic]
+    fn out_of_range_below_zero() {
+        reverse_map(-0.1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn out_of_range_above_one() {
+        reverse_map(1.1);
     }
 }
